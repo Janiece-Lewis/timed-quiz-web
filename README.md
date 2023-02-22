de3bcbd9e49ad283082853bdb78e74699ba8b7b1
Janiece-Lewis <jlewilewi@gmail.com>
2023-02-21 23:19:01 -0500

    updated functions

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
 # timed-quiz
+
+## My Task
+
+This week's Challenge was to make a quiz based on the funadamentals of JavaScript
+
+## Table of contents
+
+- [User Story](#userstory)
+- [Acceptance Criteria](#AcceptanceCriteria)
+- [Preview](#preview)
+- [Contributing](#contributing)
+- [License](#license)
+
+
+
+
+
+## User Story
+
+```
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+
+```
+
+## Acceptance Criteria
+
+```
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+```
+
+
+## Preview
+![preview image](./assets/images/screencapture-127-0-0-1-5500-index-html-2023-02-21-23_13_08.png)
+[Deployed Link]()
+
+## Contributing
+Pull requests are welcome. For major changes, please open and issue first to discuss what you would like to change.
+
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,20 @@
 let startButton = document.querySelector("#startbtn");
 let startQuizEl = document.querySelector(".ques-answers-screen");
 let timerEl = document.querySelector(".timer-count");
-let result = document.querySelector(".final-score")
+let result = document.querySelector(".final-score");
 let timer;
-let timerCount = 30
+let question = document.querySelector("#question")
+let choices = document.querySelector("#choices")
+let answer= document.querySelector("#answer")
+let score = 0;
+
+
+// let button = document.querySelector("#userChoice");
+let timerCount = 60;
+let plusPoint = 0;
+let minusPoint = 0;
+let qindex=0;
+
 
 
 
@@ -12,29 +23,51 @@ let questions = [
     {
         question :"JavaScript gives a page ________.",
         choices : ["styling","structure","function","bananas"],
-        correct:"function"
+        correct:"2"
     },
     {
         question :"JavaScript was originally called _______.",
         choices :[ "iscript","LiveScript","MyScript","CryScript"],
-        correct: "LiveScript"
+        correct: "1"
     },
     {
         question :"JavaScript is added to the HTML file in a_______tag.",
         choices : ["script","style","img","link"],
-        correct: "script"
+        correct: "0"
     },
     {
         question :"JavaScript is used for ________.",
         choices : ["apps","game development","servers","all of the above"],
-        correct:"all of the above"
+        correct:"3"
     },
     {
         question :"JavaScript is compatible with_______.",
         choices :[ "wine","pizza","other languages","bananas"],
-        correct: "other languages"
+        correct: "2"
     },
 ];
+
+
+// for (var i=0 i <questions.length; i++)
+// var response = window.prompt(question[i].promt)
+
+// if(response === questions[i].correct)
+
+
+
+
+//above line new stuff i tried
+function correctAnswer(){
+    if (questions[choices]===correct){
+        score++;
+
+    }
+
+    else{
+        minusPoint--
+    }
+    
+}
 
 
 function startTimer() {
@@ -52,13 +85,37 @@ function startTimer() {
     }, 1000);
   }
 
-
+function getQuestion(){
+    question.textContent = questions[qindex].question
+    for( let i=0 ; i<questions[qindex].choices.length;i++){
+        const button =document.createElement("button")
+        button.textContent=questions[qindex].choices[i]
+        question.append(button)
+    }
+}
 function startQuiz() {
 document.querySelector("#start").classList.add("hide")
-startQuizEl.classList.remove("hide")
+// startQuizEl.classList.remove("hide")
 startTimer()
+getQuestion()
+}
+// func to check answers
+function checkAnswers(event){
+    let userAnswer = event.target.dispatchEvent;
+    if (correct===questions[qindex].answer){
+        answer.textContent = "Great job!"
+
+    }
+    else {
+        answer.textContent = "Try Again!";
+        timerCount =-5000
+    }
+
+    
 }
 
-startButton.addEventListener("click", startQuiz);
 
+
+startButton.addEventListener("click", startQuiz);
+button.addEventListener('click',checkAnswers);
   
